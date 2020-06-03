2b7feb504d4ea18ad30b74be6332de247ceca5bf
Hellyson-Ferreira <hellysonferreira.hf1@gmail.com>
2020-06-03 20:27:38 -0300

    Dia 3 - A escolha da stack   NLW#1 Starter

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -1,29 +1,31 @@
-function polulateUfs(){
+function polulateUfs() {
     const url = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados'
     const ufSelect = document.querySelector("select[name=uf]")
-    fetch(url).then(res=> res.json() ).then(states =>{
+    fetch(url).then(res => res.json()).then(states => {
         for (const state of states) {
             ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
         }
     })
 
 }
-function getCitys(event){
-    
+function getCities(event) {
+
     const citySelect = document.querySelector("select[name=city]")
     const stateInput = document.querySelector("input[name=state]")
 
-    
+
     const ufValue = event.target.value
-    //stateInput.value = event.target.options[event.target.selectendIndex].text
+    stateInput.value = event.target.options[event.target.selectedIndex].text
+
     citySelect.innerHTM = `<option value="">Selecione a cidade</option>`
+    citySelect.disabled = true
 
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
-    fetch(url).then(res=> res.json() ).then(cities =>{
+    fetch(url).then(res => res.json()).then(cities => {
         for (const city of cities) {
-            citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+            citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
         }
-        if(ufValue!=''){
+        if (ufValue != '') {
             citySelect.disabled = false
         }
     })
@@ -32,5 +34,34 @@ function getCitys(event){
 
 polulateUfs()
 
-document.querySelector("select[name=uf]").addEventListener("change",getCitys)
+document.querySelector("select[name=uf]").addEventListener("change", getCities)
 
+const colectedItems = document.querySelector('input[name=items]')
+let selectedItems = []
+//Itens de coleta 
+function hundleSelectedItem(event) {
+
+    const itemeLi = event.target
+
+    itemeLi.classList.toggle("selected")
+
+    const itemId = event.target.dataset.id
+    const alreadySelected = selectedItems.findIndex(item => item == itemId)
+
+    if (alreadySelected != -1) {
+       const filteredItens = selectedItems.filter(item=> item != itemId)
+       selectedItems = filteredItens
+    }else{
+        selectedItems.push(itemId)
+    }
+   colectedItems.value = selectedItems
+    
+
+}
+const itemsToColect = document.querySelectorAll(".items-grid li")
+
+for (const item of itemsToColect) {
+    item.addEventListener("click", hundleSelectedItem)
+
+}
+ 
