a14d3d9caacb602d539361425934f0d2f64fde8e
Hellyson-Ferreira <hellysonferreira.hf1@gmail.com>
2020-06-06 20:55:06 -0300

    Dia 5  -  A milha extra | NLW#1 Starter

--- src/server.js
+++ src/server.js
@@ -1,11 +1,15 @@
 const express = require("express")
 const server = express()
+///pergar banco
+const db = require("./database/db.js")
+
 //configure pasta publica
 server.use(express.static("public"))
-
+////Habilirar uso do req.body
+server.use(express.urlencoded({ extended: true }))
 //Utilizando template engines
 const nunjucks = require('nunjucks')
-nunjucks.configure('src/views', { express: server, noCache:true })
+nunjucks.configure('src/views', { express: server, noCache: true })
 
 
 
@@ -16,8 +20,61 @@ server.get('/', (req, res) => {
 server.get('/create-point', (req, res) => {
     return res.render("create-point.html")
 })
+
+server.post("/savepoint", (req, res) => {
+    const valueReq = req.body
+    //     //Inserindo dados
+    const query = `
+
+            INSERT INTO places(
+                image,
+                name,
+                address,
+                address2,
+                state,
+                city,
+                items
+            )VALUES(?,?,?,?,?,?,?)
+    `
+    const values = [
+        req.body.image,
+        req.body.name,
+        req.body.address,
+        req.body.address2,
+        req.body.state,
+        req.body.city,
+        req.body.items,
+    ]
+
+    function afterInsertData(err) {
+        if (err) {
+            return console.log(err)
+
+        }
+        console.log("Cadastrado com sucesso")
+        console.log(this)
+        return res.render('create-point.html', { saved: true })
+    }
+    db.run(query, values, afterInsertData)
+
+})
+
 server.get('/search-results', (req, res) => {
-    return res.render("search-results.html")
+    const search = req.query.search
+    if(search == ""){
+        return res.render("search-results.html", {total: 0})
+    }
+
+    db.all(`SELECT * FROM places WHERE  city LIKE '%${search}%'`, function (err, rows) { 
+        if (err) {
+            return console.log(err)
+        }
+        const total = rows.length
+        //console.log(rows)
+        return res.render("search-results.html", { places: rows, total: total })
+    })
+
+
 })
 
 
